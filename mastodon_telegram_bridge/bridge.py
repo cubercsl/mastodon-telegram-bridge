e14830d3595cf83ae71648b9d03a7869ba27f86e
cubercsl <2014cais01@gmail.com>
2022-12-21 12:58:35 +0800

    feat: add forward_tags filter

--- mastodon_telegram_bridge/bridge.py
+++ mastodon_telegram_bridge/bridge.py
@@ -12,18 +12,19 @@ from mastodon_telegram_bridge import format_exception, logger, markdownify, Mast
 
 class Bridge:
     def __init__(self, *,
-                 tg_bot_token: str,
-                 channel_chat_id: int,
-                 pm_chat_id: int,
-                 mastodon_host: str,
-                 mastodon_api_access_token: str,
-                 mastodon_app_name: str,
-                 mastodon_username: str,
-                 scope: List[str],
-                 add_link_in_telegram: bool,
-                 add_link_in_mastodon: bool,
-                 show_forward_info: bool,
-                 noforward_tags: List[str]) -> None:
+                 tg_bot_token: str = '',
+                 channel_chat_id: int = 0,
+                 pm_chat_id: int = 0,
+                 mastodon_host: str = '',
+                 mastodon_api_access_token: str = '',
+                 mastodon_app_name: str = '',
+                 mastodon_username: str = '',
+                 scope: List[str] = None,
+                 add_link_in_telegram: bool = True,
+                 add_link_in_mastodon: bool = False,
+                 show_forward_info: bool = True,
+                 forward_tags: List[str] = None,
+                 noforward_tags: List[str] = None) -> None:
         self.mastodon = Mastodon(access_token=mastodon_api_access_token,
                                  api_base_url=mastodon_host)
         self.bot = Bot(token=tg_bot_token)
@@ -34,11 +35,14 @@ class Bridge:
 
         # get the username of the mastodon account
         self.mastodon_username = self.mastodon.account_verify_credentials().username
-        if mastodon_username is not None and mastodon_username != self.mastodon_username:
+        if mastodon_username and mastodon_username != self.mastodon_username:
             logger.warning('The username in the config file (%s) does not match the username of the mastodon account (%s).',
                            mastodon_username, self.mastodon_username)
         self.mastodon_app_name = mastodon_app_name
         logger.info('Username: %s, App name: %s', self.mastodon_username, self.mastodon_app_name)
+
+        if forward_tags and noforward_tags:
+            logger.warning('Both forward_tags and noforward_tags are set, noforward_tags will be ignored.')
 
         self.cfg = AttribAccessDict(
             # get the chat id of the telegram channel and error reporting chat
@@ -46,8 +50,14 @@ class Bridge:
             pm_chat_id=pm_chat_id,
             # filter out the messages that should not be forwarded
             scope=scope,
+            forward_tags=forward_tags,
             noforward_tags=noforward_tags,
         )
+
+    def _should_forward(sef, msg: str) -> bool:
+        if sef.cfg.forward_tags:
+            return any(tag in msg for tag in sef.cfg.forward_tags)
+        return not any(tag in msg for tag in sef.cfg.noforward_tags)
 
     def _should_forward_to_telegram(self, status: AttribAccessDict) -> bool:
         # check if the message is from the telegram channel or another app
@@ -69,11 +79,12 @@ class Bridge:
 
             if channel_post.photo:
                 if channel_post.media_group_id:
+                    # TODO: support media group
                     logger.info('This is a media group. Skip it.')
                     return
                 with TemporaryDirectory(prefix='mastodon') as tmpdir:
                     text = channel_post.caption or ''
-                    if any(tag in text for tag in self.cfg.noforward_tags):
+                    if not self._should_forward(text):
                         logger.info('Do not forward this channel message to mastodon.')
                         return
                     file_path = f'{tmpdir}/{channel_post.photo[-1].file_unique_id}'
@@ -82,7 +93,7 @@ class Bridge:
             elif channel_post.video:
                 with TemporaryDirectory(prefix='mastodon') as tmpdir:
                     text = channel_post.caption or ''
-                    if any(tag in text for tag in self.cfg.noforward_tags):
+                    if not self._should_forward(text):
                         logger.info('Do not forward this channel message to mastodon.')
                         return
                     file_path = f'{tmpdir}/{channel_post.video.file_name}'
@@ -90,7 +101,7 @@ class Bridge:
                     media_ids = self.mastodon.media_post(file_path).id
             elif channel_post.text:
                 text = channel_post.text
-                if any(tag in text for tag in self.cfg.noforward_tags):
+                if not self._should_forward(text):
                     logger.info('Do not forward this channel message to mastodon.')
                     return
             else:
@@ -142,17 +153,20 @@ class Bridge:
         logger.warning('Update "%s" caused error "%s"', update, context.error)
         logger.exception(context.error)
 
-    def run(self) -> None:
+    def run(self, *,
+            telegram_to_mastodon: bool = True,
+            mastodon_to_telegram: bool = True) -> None:
         # Mastodon stream
-        listener = CallbackStreamListener(
-            update_handler=self._send_message_to_telegram)
-        self.mastodon.stream_user(listener=listener, run_async=True)
+        if mastodon_to_telegram:
+            listener = CallbackStreamListener(update_handler=self._send_message_to_telegram)
+            self.mastodon.stream_user(listener=listener, run_async=True)
 
         # Telegram bot
         updater = Updater(bot=self.bot, use_context=True)
         dispatcher: Dispatcher = updater.dispatcher
         dispatcher.add_error_handler(self._error)
         dispatcher.add_handler(CommandHandler('start', self._start))
-        dispatcher.add_handler(MessageHandler(Filters.update.channel_post, self._send_message_to_mastodon))
+        if telegram_to_mastodon:
+            dispatcher.add_handler(MessageHandler(Filters.update.channel_post, self._send_message_to_mastodon))
         updater.start_polling()
         updater.idle()
--- mastodon_telegram_bridge/main.py
+++ mastodon_telegram_bridge/main.py
@@ -10,6 +10,8 @@ def main():
     parser.add_argument('config', help='config file path', nargs='?', default='config.json')
     parser.add_argument('-v', '--verbose', help='verbose mode', action='store_true')
     parser.add_argument('-d', '--debug', help='debug mode', action='store_true')
+    parser.add_argument('--disable-send-to-mastodon', help='disable sending messages to mastodon', action='store_true')
+    parser.add_argument('--disable-send-to-telegram', help='disable sending messages to telegram', action='store_true')
     args = parser.parse_args()
     if args.verbose:
         level = logging.INFO
@@ -23,7 +25,15 @@ def main():
                         handlers=[logging.StreamHandler()])
     with open(args.config) as cfg:
         config = json.load(cfg)
-    Bridge(**config).run()
+    if args.disable_send_to_mastodon and args.disable_send_to_telegram:
+        raise ValueError('Both --disable-send-to-mastodon and --disable-send-to-telegram are set, do nothing.')
+    telegram_to_mastodon = True
+    mastodon_to_telegram = True
+    if args.disable_send_to_mastodon:
+        telegram_to_mastodon = False
+    if args.disable_send_to_telegram:
+        mastodon_to_telegram = False
+    Bridge(**config).run(telegram_to_mastodon=telegram_to_mastodon, mastodon_to_telegram=mastodon_to_telegram)
 
 
 if __name__ == '__main__':
